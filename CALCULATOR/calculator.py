3fa02646ccdbd60e34361549689d83cbf793c8c1
HP <cnlabidi@gmail.com>
2025-10-29 10:47:15 +0200

    Blue(refactory)

--- CALCULATOR/calculator.py
+++ CALCULATOR/calculator.py
@@ -1,2 +1,2 @@
-def add():
+def add(n):
     return 0
--- CALCULATOR/test_calculator.py
+++ CALCULATOR/test_calculator.py
@@ -1,5 +1,8 @@
 import pytest
 from CALCULATOR.calculator import add
 
-def test_empty_string_return_0():
-    assert add("") == 0
+@pytest.mark.parametrize("n, expected", [
+    ("", 0),
+])
+def test_add(n, expected):
+    assert add(n) == expected
