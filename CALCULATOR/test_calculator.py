4916b135ac85817191d55a87eae69912733b45c6
HP <cnlabidi@gmail.com>
2025-10-29 17:21:47 +0200

    Red (failed test)

--- CALCULATOR/test_calculator.py
+++ CALCULATOR/test_calculator.py
@@ -4,6 +4,7 @@ from CALCULATOR.calculator import add
 @pytest.mark.parametrize("n, expected", [
     ("", 0),
     ("1", 1),
+    ("0,1", 1),
 ])
 def test_add(n, expected):
     assert add(n) == expected
